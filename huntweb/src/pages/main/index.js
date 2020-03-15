0d37bad8a469c598f30bd4f145c77760395f2ba1
Iury <iury@rarolabs.com.br>
2020-03-15 13:45:16 -0300

    Listando produtos

--- huntweb/src/pages/main/index.js
+++ huntweb/src/pages/main/index.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import api from '../../services/api';
 
+import './styles.css';
+
 export default class Main extends Component {
     state = {
         products: []
@@ -17,12 +19,17 @@ export default class Main extends Component {
     }
 
     render() {
-    return (
-        <div className="product-list">
-            {this.state.products.map(product => (
-                <h2 key={product._id}>{product.title}</h2>
-            ))}
-        </div>
-    );
+        const { products } = this.state; 
+        return (
+            <div className="product-list">
+                {products.map(product => (
+                    <article key={product._id}>
+                        <strong>{product.title}</strong>
+                        <p>{product.description}</p>
+                        <a href={product.url}>Acessar</a>
+                    </article>
+                ))}
+            </div>
+        );
     }
 }
